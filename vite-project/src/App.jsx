4b990018379ac1bb2ea09ec4bd85fe9e82fae11a
Mihiri <110478627+MIHIRI099@users.noreply.github.com>
2023-10-05 21:22:33 +0530

    Header_bar_ui_icon_search_bar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,28 @@ import './App.css'
 
 function App() {
   return (
-      <div className="bg-red-500">test</div>  
+      <div>
+        <header className='p-4 flex justify-between'>
+          <a href="" className='flex items-center gap-1'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+            </svg>
+            <span className='font-bold text-xl'>Tucker</span>
+          </a>
+          <div className='flex gap-1 border border-red-300 rounded-full py-2 px-4 shadow-md shadow-red-200'>
+            <div></div>
+            <div className='px-4'>Search here......</div>
+            <div className="border-l border-red-300 "></div>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+            </button>
+            
+        
+          </div>
+        </header>
+      </div>  
   )
 }
 
