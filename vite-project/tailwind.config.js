0a7ef32044e8cd50acab67220e7142ed795aa334
Mihiri <110478627+MIHIRI099@users.noreply.github.com>
2023-10-06 12:54:58 +0530

    ui_headbar_updates

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          primary : '#FF8370'
+
+
+      }
+    },
   },
   plugins: [],
 }
