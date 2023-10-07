2342e3d5ba2ef364b1a047224fdb98e0ee4b3a89
Mihiri <110478627+MIHIRI099@users.noreply.github.com>
2023-10-08 00:34:26 +0530

    LoginPage_design

--- vite-project/src/pages/LoginPage.jsx
+++ vite-project/src/pages/LoginPage.jsx
@@ -1,4 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
-    return ("Login Page")
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+        <div className="mb-60">
+            <h1 className="text-4xl text-center mb-4">Login</h1>
+            <form className="max-w-md mx-auto ">
+                <label htmlFor="">Email</label>
+                <input type="email" placeholder="your@gmail.com"/>
+                <label htmlFor="">Password</label>
+                <input type="password" placeholder="password" />
+                <button className="primary">Login</button>
+                <div className="text center py-2 ml-12 text-gray-500">Do not have an account yet?  
+                <Link className="underline text-black" to={'/register'}> Register Now</Link>
+                </div>
+            </form>
+        </div>
+        </div>
+    )
 
 }
