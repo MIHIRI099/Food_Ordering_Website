8e2ae968075247fb8906ad30e84e401ec3a4a6d0
Mihiri <110478627+MIHIRI099@users.noreply.github.com>
2023-10-07 23:44:26 +0530

    Header_layout

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import {Route,Routes} from "react-router-dom"
 import Index from "./pages/Index"
+import LoginPage from './pages/LoginPage'
+import Layout from './Layout'
 
 function App() {
   return (
       <Routes>
+        <Route path='/' element ={<Layout/>}>
         <Route index element={<Index/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        </Route>
      </Routes> 
      )
     }
---
+++ vite-project/src/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+export default function Layout(){
+return (
+    <div className="p-4">
+        <Header/>
+        <Outlet/>
+
+    </div>
+
+)
+
+
+}
